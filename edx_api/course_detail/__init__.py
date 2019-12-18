23a5d76b7bc54cee4b64917055dc0f6d6d062bda
sar <sar.haidar@gmail.com>
2019-12-18 14:46:37 -0500

    Update CourseDetails client to pass username

--- edx_api/course_detail/__init__.py
+++ edx_api/course_detail/__init__.py
@@ -13,7 +13,7 @@ class CourseDetails(object):
         self._requester = requester
         self._base_url = base_url
 
-    def get_detail(self, course_id):
+    def get_detail(self, course_id, username=None):
         """
         Fetches course details.
 
@@ -23,13 +23,22 @@ class CourseDetails(object):
         Returns:
             CourseDetail
         """
-        # the request is done in behalf of the current logged in user
-        resp = self._requester.get(
-            urljoin(
-                self._base_url,
-                '/api/courses/v1/courses/{course_key}/'.format(course_key=course_id)
-            )
-        )
+        # the request is done on behalf of the current logged in user
+        # this only works if COURSE_ABOUT_VISIBILITIY_PERMISSION is not
+        # set to staff, otherwise you need to pass in a username with
+        # permissions.
+        if not username:
+            resp = self._requester.get(urljoin
+                                       (self._base_url,
+                                        '/api/courses/v1/courses/{course_key}'
+                                        .format(course_key=course_id)))
+        else:
+            resp = self._requester.get(urljoin
+                                       (self._base_url,
+                                        '/api/courses/v1/courses/{course_key}/'
+                                        '?username={username}'
+                                        .format(course_key=course_id,
+                                                username=username)))
 
         resp.raise_for_status()
 
