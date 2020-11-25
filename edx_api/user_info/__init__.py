815a3ee5e94977a94bf174f2fe2a79444a4778c8
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2020-11-25 16:09:24 +0500

    Added api to update user's name in edX

--- edx_api/user_info/__init__.py
+++ edx_api/user_info/__init__.py
@@ -36,3 +36,34 @@ class UserInfo(object):
         resp.raise_for_status()
 
         return Info(resp.json())
+
+    def update_user_name(self, username, user_full_name):
+        """
+        Updates Full-Name of the user
+
+        Args:
+            username (user.models.User): Username of the Application user
+            user_full_name: Full name to be replace/updated with old full name
+
+        Returns:
+            UserInfo: Object with user details for whom the name is updated
+        """
+        user_name_data = dict(name=user_full_name)
+
+        # the request is done on behalf of the current logged in user
+        self.requester.headers.update(
+            {
+                "Accept": "application/json, text/javascript, */*; q=0.01",
+                "X-Requested-With": "XMLHttpRequest",
+                "Content-Type": "application/merge-patch+json",
+            }
+        )
+        resp = self.requester.patch(
+            urljoin(
+                self.base_url,
+                '/api/user/v1/accounts/{username}'.format(username=username)
+            ),
+            json=user_name_data)
+        resp.raise_for_status()
+
+        return Info(resp.json())
