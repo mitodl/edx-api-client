7628fdc7c8857819215de3ff85f144e8d6c994b3
Mudassir Hafeez <mudasir5761@gmail.com>
2024-08-13 17:46:41 +0500

    fix: tests using parametrized

--- edx_api/user_validation/init_test.py
+++ edx_api/user_validation/init_test.py
@@ -2,60 +2,51 @@
 Test responses from user_validation api.
 """
 
-import json
-import os
-from unittest import TestCase
+import pytest
 from urllib.parse import urljoin
-
-import requests_mock
 
 from edx_api.client import EdxApi
 from .models import UserValidationResult
 
 
-class UserValidationTestCase(TestCase):
+@pytest.mark.parametrize(
+    "expected_validation_decisions, request_data, mock_response",
+    [
+        (
+            {'name': ''},
+            {'name': 'test_name'},
+            {"validation_decisions": {"name": ""}}
+        ),
+        (
+            {'name': 'Invalid name'},
+            {'name': 'http://test_name'},
+            {"validation_decisions": {"name": "Invalid name"}}
+        ),
+        (
+            {'username': ''},
+            {'username': 'test_username'},
+            {"validation_decisions": {"username": ""}}
+        ),
+        (
+            {'username': 'Invalid username'},
+            {'username': '!test_username'},
+            {"validation_decisions": {"username": "Invalid username"}}
+        ),
+    ]
+)
+def test_validate_user_registration_info(requests_mock, expected_validation_decisions, request_data, mock_response):
     """
-    Tests for the UserValidation API client.
+    Test that validate_user_registration_info validates name and username.
     """
-
     base_url = "https://edx.example.com"
+    client = EdxApi({"access_token": ""}, base_url)
 
-    def setUp(self):
-        super().setUp()
+    requests_mock.post(
+        urljoin(base_url, '/api/user/v1/validation/registration'),
+        json=mock_response
+    )
 
-        with open(
-            os.path.join(
-                os.path.dirname(__file__),
-                "fixtures/user_validation.json",
-            )
-        ) as file:  # pylint: disable=redefined-builtin
-            self.validation_data = json.load(file)
-        self.client = EdxApi({"access_token": ""}, self.base_url)
+    validation_response = client.user_validation.validate_user_registration_info(request_data)
 
-    def get_mock_response(self, key):
-        """
-        Return the validation_decisions response from the fixture.
-        """
-        return self.validation_data['validation_responses'].get(key, {})
-
-    @requests_mock.Mocker()
-    def test_validate_user_registration_info(self, mock_req):
-        """
-        Test that validate_user_registration_info validates name and username.
-        """
-        test_cases = [
-            ('valid_name', {'name': ''}, {'name': 'test_name'}),
-            ('invalid_name', {'name': 'Invalid name'}, {'name': 'http://test_name'}),
-            ('valid_username', {'username': ''}, {'username': 'test_username'}),
-            ('invalid_username', {'username': 'Invalid username'}, {'username': '!test_username'}),
-        ]
-
-        for key, expected_validation_decisions, request_data in test_cases:
-            with self.subTest(key=key):
-                mock_req.post(
-                    urljoin(self.base_url, '/api/user/v1/validation/registration'),
-                    json=self.get_mock_response(key)
-                )
-                validation_response = self.client.user_validation.validate_user_registration_info(request_data)
-                self.assertIsInstance(validation_response, UserValidationResult)
-                self.assertDictEqual(validation_response.validation_decisions, expected_validation_decisions)
+    assert isinstance(validation_response, UserValidationResult)
+    assert validation_response.validation_decisions == expected_validation_decisions
--- edx_api/user_validation/models_test.py
+++ edx_api/user_validation/models_test.py
@@ -1,42 +1,29 @@
-"""Tests for Validation"""
-import json
-import os
-from unittest import TestCase
+"""Tests for UserValidationResult"""
+
+import pytest
 
 from .models import UserValidationResult
 
 
-class UserValidationResultTests(TestCase):
-    """Tests for UserValidationResult"""
+@pytest.mark.parametrize(
+    "expected_value, validation_data, field",
+    [
+        ('Invalid name', {'validation_decisions': {'name': 'Invalid name'}}, 'name'),
+        ('', {'validation_decisions': {'name': ''}}, 'name'),
+        ('Invalid username', {'validation_decisions': {'username': 'Invalid username'}}, 'username'),
+        ('', {'validation_decisions': {'username': ''}}, 'username'),
+    ]
+)
+def test_properties(expected_value, validation_data, field):
+    """Test properties on UserValidationResult model"""
+    validation = UserValidationResult(validation_data)
+    if field == 'name':
+        assert validation.name == expected_value
+    elif field == 'username':
+        assert validation.username == expected_value
 
-    @classmethod
-    def setUpClass(cls):
-        with open(os.path.join(os.path.dirname(__file__),
-                               'fixtures/user_validation.json')) as file_obj:
-            cls.user_validation_json = json.loads(file_obj.read())
 
-        cls.validation_responses = cls.user_validation_json.get('validation_responses', {})
-
-    def test_str(self):
-        """Test the __str__"""
-        self.assertEqual(str(self.get_validation_instance('valid_name')), "<User validation>")
-
-    def test_properties(self):
-        """Test properties on UserValidationResult model"""
-        test_cases = [
-            ('invalid_name', 'Invalid name', 'name'),
-            ('valid_name', '', 'name'),
-            ('invalid_username', 'Invalid username', 'username'),
-            ('valid_username', '', 'username'),
-        ]
-
-        for key, expected_value, field in test_cases:
-            with self.subTest(key=key):
-                validation = self.get_validation_instance(key)
-                if field == 'name':
-                    self.assertEqual(validation.name, expected_value)
-                elif field == 'username':
-                    self.assertEqual(validation.username, expected_value)
-
-    def get_validation_instance(self, key):
-        return UserValidationResult(self.validation_responses.get(key, {}))
+def test_str():
+    """Test the __str__ method of UserValidationResult"""
+    validation = UserValidationResult({'validation_decisions': {'name': ''}})
+    assert str(validation) == "<User validation>"
