c0911379fb801f0de6ae54ab126fef6e5891ded8
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2025-05-14 15:29:49 +0500

    add update course run API and refactor create API

--- edx_api/course_runs/__init__.py
+++ edx_api/course_runs/__init__.py
@@ -15,6 +15,38 @@ class CourseRuns:
         self._requester = requester
         self._base_url = base_url
 
+    def _verify_and_generate_schedule(self, start, end, enrollment_start, enrollment_end):
+        """
+        Verifies and builds the schedule dictionary for course run creation.
+
+        Args:
+            start (datetime): The start date for the course run.
+            end (datetime): The end date for the course run.
+            enrollment_start (datetime): The enrollment start date for the course run.
+            enrollment_end (datetime): The enrollment end date for the course run.
+
+        Returns:
+            dict: A dictionary containing the schedule information.
+        """
+
+        # All the date fields will be added in a sub-dictionary named "schedule".
+        # If we want to update the schedule of the course, the start and end dates
+        # are required by the API.
+        if start or end:
+            if not (start and end):
+                raise ValueError("Both start and end dates must be provided if one is provided.")
+
+            schedule = {
+                "start": start.isoformat(),
+                "end": end.isoformat(),
+            }
+            if enrollment_start:
+                schedule["enrollment_start"] = enrollment_start.isoformat()
+            if enrollment_end:
+                schedule["enrollment_end"] = enrollment_end.isoformat()
+            return schedule
+        return None
+
     def clone_course_run(self, source_course_id, destination_course_id):
         """
         Clones a course run from source_course_id in Open edX.
@@ -53,9 +85,14 @@ class CourseRuns:
             number (str): Course number for the new course run. (Without 'course-v1')
             run (str): The run id for the new course run.
             title (str): The title of the new course run.
+            pacing_type (str, optional): The pacing type for the new course run. Defaults to None.
+            start (datetime, optional): The start date for the new course run. Defaults to None.
+            end (datetime, optional): The end date for the new course run. Defaults to None.
+            enrollment_start (datetime, optional): The enrollment start date for the new course run. Defaults to None.
+            enrollment_end (datetime, optional): The enrollment end date for the new course run. Defaults to None.
 
         Returns:
-            Response: The response from the Open edX API.
+            CourseRun: The course run fields from the Open edX API.
         Raises:
             CourseRunError: If the request to clone the course run fails.
         """
@@ -65,16 +102,17 @@ class CourseRuns:
             "number": number,
             "run": run,
             "title": title,
-            "schedule": {
-                "start": start.isoformat() if start else None,
-                "end": end.isoformat() if end else None,
-                "enrollment_start": enrollment_start.isoformat() if enrollment_start else None,
-                "enrollment_end": enrollment_end.isoformat() if enrollment_end else None,
-            },
         }
         if pacing_type:
             payload["pacing_type"] = pacing_type
-        
+
+        # All the date fields should be added in a sub-dictionary named "schedule" inside the payload.
+        schedule = self._verify_and_generate_schedule(
+            start, end, enrollment_start, enrollment_end
+        )
+        if schedule:
+            payload["schedule"] = schedule
+
         resp = self._requester.post(
             parse.urljoin(self._base_url, "api/v1/course_runs/"), json=payload
         )
@@ -84,4 +122,47 @@ class CourseRuns:
         except HTTPError as e:
             raise CourseRunAPIError(
                 f"Failed to create course run: {e.response.status_code} - {e.response.text}"
+            ) from e
+
+    def update_course_run(self, course_id, title=None, pacing_type=None, start=None, end=None, enrollment_start=None, enrollment_end=None):
+        """
+        Creates a new canonical course run in Open edX.
+
+        Args:
+            course_id (str): Course Id for the course run to update.
+            title (str, optional): The title of the new course run.
+            pacing_type (str, optional): The pacing type for the new course run. Defaults to None.
+            start (datetime, optional): The start date for the new course run. Defaults to None.
+            end (datetime, optional): The end date for the new course run. Defaults to None.
+            enrollment_start (datetime, optional): The enrollment start date for the new course run. Defaults to None.
+            enrollment_end (datetime, optional): The enrollment end date for the new course run. Defaults to None.
+
+        Returns:
+            CourseRun: The course run object containing the fields data from the Open edX API.
+        Raises:
+            CourseRunError: If the request to clone the course run fails.
+        """
+        # the request is done on behalf of the staff user
+        payload = {}
+        if title:
+            payload["title"] = title
+        if pacing_type:
+            payload["pacing_type"] = pacing_type
+
+        # All the date fields should be added in a sub-dictionary named "schedule" inside the payload.
+        schedule = self._verify_and_generate_schedule(
+            start, end, enrollment_start, enrollment_end
+        )
+        if schedule:
+            payload["schedule"] = schedule
+
+        resp = self._requester.put(
+            parse.urljoin(self._base_url, f"api/v1/course_runs/{course_id}/"), json=payload
+        )
+        try:
+            resp.raise_for_status()
+            return CourseRun(resp.json())
+        except HTTPError as e:
+            raise CourseRunAPIError(
+                f"Failed to update course run: {e.response.status_code} - {e.response.text}"
             ) from e
